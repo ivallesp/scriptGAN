88554ff265090660273d903573e9644d5212f91a
Ivan Valles Perez <ivan_valles_perez@mckinsey.com>
2018-01-09 07:20:34 +0100

    Replaced the embedding rnn of the generator by a normal RNN with dense layers afterwards and adapted the main script to run the TATOEBA_V2

--- src/architecture_embedding.py
+++ src/architecture_embedding.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from src.nn_frankenstein.activations import leaky_relu
-from src.nn_frankenstein.embedding_decoder import build_lstm_feed_back_layer
+from src.nn_frankenstein.decoder import build_lstm_feed_back_layer
 from src.nn_frankenstein.normalization import BatchNorm
 
 
@@ -45,24 +45,31 @@ def build_discriminator(input_, reuse=False):
     return x
 
 
-def build_generator(z, n_neurons_rnn, max_length, vocabulary_size):
+def build_generator(z, max_length, batch_size, vocabulary_size):
     with tf.variable_scope("Generator", reuse=False):
+        bn_z = BatchNorm(name="batch_normalization_z")
         bn_zh = BatchNorm(name="batch_normalization_zh")
         bn_zc = BatchNorm(name="batch_normalization_zc")
+        bn_d_1 = BatchNorm(name="batch_normalization_d_1")
+        bn_d_2 = BatchNorm(name="batch_normalization_d_2")
+        bn_d_3 = BatchNorm(name="batch_normalization_d_3")
 
-        z_norm = z
-        zh_projected = tf.layers.dense(z_norm, n_neurons_rnn, activation=None,
-                                       kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                       name="dense_zh_projection")
-        zc_projected = tf.layers.dense(z_norm, n_neurons_rnn, activation=None,
-                                       kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                       name="dense_zc_projection")
+        z_norm = bn_z(z)
+        zh_projected = tf.layers.dense(z_norm, 1024, activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense_zh_projection")
+        zc_projected = tf.layers.dense(z_norm, 1024, activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense_zc_projection")
 
         output_dec, states_dec = build_lstm_feed_back_layer(bn_zh(zh_projected), bn_zc(zc_projected),
-                                                            max_length=max_length, cardinality_input=vocabulary_size,
-                                                            name="gen_feed_back")
+                                                            max_length=max_length, name="gen_feed_back")
 
-    return(output_dec)
+        lstm_stacked_output = tf.reshape(output_dec, shape=[-1, output_dec.shape[2].value], name="g_stack_LSTM")
+        d = tf.layers.dense(bn_d_1(lstm_stacked_output), 512, activation=leaky_relu, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense_1")
+        d = tf.layers.dense(bn_d_2(d), 1024, activation=leaky_relu, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense_2")
+        d = tf.layers.dense(bn_d_3(d), vocabulary_size, activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense_3")
+
+        unstacked_output = tf.reshape(d, shape=[batch_size, max_length, vocabulary_size], name="g_unstack_LSTM")
+        o=tf.nn.softmax(unstacked_output)
+    return(o)
+
 
 
 
@@ -80,7 +87,7 @@ class GAN:
         self.noise_depth = noise_depth
         self.vocabulary_size = vocabulary_size
         self.optimizer_generator = tf.train.AdamOptimizer(learning_rate=0.00001)
-        self.optimizer_discriminator = tf.train.AdamOptimizer(learning_rate=0.00001)
+        self.optimizer_discriminator = tf.train.AdamOptimizer(learning_rate=0.00015)
         self.define_computation_graph()
 
         # Aliases
@@ -109,8 +116,9 @@ class GAN:
     def define_core_model(self):
         with tf.variable_scope("Core_Model"):
             tweet = tf.one_hot(self.placeholders.codes_in, depth=self.vocabulary_size)
-            G = build_generator(z=self.placeholders.z, n_neurons_rnn=self.n_neurons_rnn_gen,
+            G = build_generator(z=self.placeholders.z,
                                 max_length=self.max_length,
+                                batch_size=self.batch_size,
                                 vocabulary_size=self.vocabulary_size)
             D_real = build_discriminator(input_=tweet)
             D_fake = build_discriminator(input_=G, reuse=True)
