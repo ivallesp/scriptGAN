fe797a348c1ebd0dca5906992d9914202a49f7cd
Ivan Valles Perez <ivanvallesperez@gmail.com>
2018-01-18 08:24:16 +0100

    V15_MultiRNN prepared

--- src/architecture_embedding.py
+++ src/architecture_embedding.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from src.nn_frankenstein.activations import leaky_relu
-from src.nn_frankenstein.decoder import build_lstm_feed_back_layer
+from src.nn_frankenstein.decoder_multirnn import build_lstm_feed_back_layer
 from src.nn_frankenstein.normalization import BatchNorm
 
 
@@ -87,7 +87,7 @@ class GAN:
         self.noise_depth = noise_depth
         self.vocabulary_size = vocabulary_size
         self.optimizer_generator = tf.train.AdamOptimizer(learning_rate=0.00001)
-        self.optimizer_discriminator = tf.train.AdamOptimizer(learning_rate=0.00015)
+        self.optimizer_discriminator = tf.train.AdamOptimizer(learning_rate=0.00005)
         self.define_computation_graph()
 
         # Aliases
