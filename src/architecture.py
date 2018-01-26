137e3cf4905d2c53cbcc124f5569fb2f2043653e
Ivan Valles Perez <ivan_valles_perez@mckinsey.com>
2018-01-26 15:05:17 +0100

    Discriminator LSTM axes adjusted and type hidden states

--- src/architecture.py
+++ src/architecture.py
@@ -28,16 +28,16 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         dtype = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-        self.recurrent_hidden = (autograd.Variable(torch.zeros(1, batch_size, 1024)),
-                                 autograd.Variable(torch.zeros((1, batch_size, 1024))))
+        self.recurrent_hidden = (autograd.Variable(torch.zeros(1, batch_size, 1024).type(dtype)),
+                                 autograd.Variable(torch.zeros((1, batch_size, 1024).type(dtype))))
         self.rnn = nn.LSTM(channels_in, 1024)
         self.d_1 = nn.Linear(1024, 512)
         self.d_2 = nn.Linear(512, 256)
         self.d_3 = nn.Linear(256, 1)
 
     def forward(self, x):
-        lstm_out, _ = self.rnn(x, self.recurrent_hidden)
-        lstm_out = lstm_out[:,:,-1]
+        lstm_out, _ = self.rnn(x.permute(2,0,1), self.recurrent_hidden)
+        lstm_out = lstm_out.permute(1,2,0)[:,:,-1]
         o_1 = nn.LeakyReLU()(self.d_1(lstm_out))
         o_2 = nn.LeakyReLU()(self.d_2(o_1))
         o_3 = self.d_3(o_2)
