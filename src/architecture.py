e73df4f16a777aee8f3c7d9420112e40021fc54d
Ivan Valles Perez <ivan_valles_perez@mckinsey.com>
2018-01-26 22:55:30 +0100

    Simplified Discriminator

--- src/architecture.py
+++ src/architecture.py
@@ -30,17 +30,15 @@ class Discriminator(nn.Module):
 
         self.recurrent_hidden = (autograd.Variable(torch.zeros(1, batch_size, 1024).type(dtype)),
                                  autograd.Variable(torch.zeros(1, batch_size, 1024).type(dtype)))
-        self.rnn = nn.LSTM(channels_in, 1024)
-        self.d_1 = nn.Linear(1024, 512)
-        self.d_2 = nn.Linear(512, 256)
-        self.d_3 = nn.Linear(256, 1)
+        self.rnn = nn.LSTM(channels_in, 256)
+        self.d_1 = nn.Linear(256, 256)
+        self.d_2 = nn.Linear(256, 1)
 
     def forward(self, x):
         lstm_out, _ = self.rnn(x.permute(2,0,1), self.recurrent_hidden)
         lstm_out = lstm_out.permute(1,2,0)[:,:,-1]
         o_1 = nn.LeakyReLU()(self.d_1(lstm_out))
-        o_2 = nn.LeakyReLU()(self.d_2(o_1))
-        o_3 = self.d_3(o_2)
+        o_3 = self.d_2(o_1)
         return o_3
 
 
