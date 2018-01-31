6c73607a49286633d42f6806676f16cba0997014
Ivan Valles Perez <ivan_valles_perez@mckinsey.com>
2018-01-31 11:06:41 +0100

    Version name and batch size adapted

--- src/main.py
+++ src/main.py
@@ -13,9 +13,9 @@ from src.text_tools import *
 
 # Define parameters
 project_id = "GAN_TATOEBA"
-version_id = "VDev"
+version_id = "V36"
 logs_path = get_tensorboard_logs_path()
-batch_size = 32
+batch_size = 512
 critic_its = 10
 noise_depth = 100
 batches_test = 10
